8187ce7e55be3c9057234d8825864d5d22b0f7a5
Dagorik <dagorik@gmail.com>
2018-05-31 20:34:48 -0700

    add arrwo

---
+++ arrow.js
@@ -0,0 +1,7 @@
+function suma(a,b){   return a + b}
+var suma1 = function (a,b) {return a + b} //anonima
+var suma2 = (a,b) =>  0 //anonima
+var suma2 = () =>  a + b //Sin parametros
+var suma2 = b =>  b * 2 //Un parametro
+var x = suma2(6,6)
+console.log(x)
